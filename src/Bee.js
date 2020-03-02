c9078e2edf6f2b466a6d95d8da6f06829f390e28
Koby Conrad <52094714+kobyconrad@users.noreply.github.com>
2020-03-01 16:11:59 -0800

    Complete Bee

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,11 @@
 var Bee = function() {
+  Grub.call(this);
+  this.age = 5;
+  this.color = "yellow";
+  // food property inherited
+  // eat method inherited
+  this.job = "keep on growing";
 };
 
+Bee.prototype = Object.create(Grub.prototype);
+Bee.prototype.constructor = Bee;
